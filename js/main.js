166a58f139ae1076f7682915be06ff87efc0e887
GenaroActis <113357267+GenaroActis@users.noreply.github.com>
2022-11-24 08:47:09 -0300

    ult

--- js/main.js
+++ js/main.js
@@ -1,17 +1,147 @@
-
 const stockProductos = [
-    { id:1, nombre:"chomba1", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:2, nombre:"chomba2", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:3, nombre:"chomba3", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:4, nombre:"chomba4", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:5, nombre:"chomba5", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:6, nombre:"chomba6", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:7, nombre:"chomba7", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-    { id:8, nombre:"chomba8", precio:4000, img:"../img/2.png", stock: 10, talle:"L"},
-]   
-const productos = document.querySelector("#productos")
+    { id:1, nombre:"chomba1", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:2, nombre:"chomba2", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:3, nombre:"chomba3", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:4, nombre:"chomba4", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:5, nombre:"chomba5", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:6, nombre:"chomba6", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:7, nombre:"chomba7", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+    { id:8, nombre:"chomba8", precio:4000, img:"../img/2.png", stock: 1, talle:"L"},
+] 
+const productos = document.querySelector("#productos");
 let carrito = [];
-let totalCantidad = 20
+for(let producto of stockProductos){
+    
+    const divCard = document.createElement('div');
+    const img = document.createElement('img');
+    const divCardBody = document.createElement('div');
+    const h5tag = document.createElement('h5');
+    const ptagTalle = document.createElement('p');
+    const ptagPrecio = document.createElement('p');
+    const atag = document.createElement('button');
+    const tagTalle = document.createElement('p');
+
+    divCard.classList.add('card','text-dark','mt-5')
+    img.classList.add('card-img-top','mt-2')
+    img.src = `${producto.img}`;
+    divCardBody.classList.add('card-body');
+    h5tag.classList.add('card-title');
+    tagTalle.classList.add('card-text');
+    ptagPrecio.classList.add('card-text');
+    atag.classList.add('btn','btn-outline-primary')
+    atag.setAttribute('onclick','sumaProducto(id)');
+
+    h5tag.innerHTML= `${producto.nombre}`;
+    ptagTalle.innerHTML= `${producto.talle}`;
+    ptagPrecio.innerHTML= `${producto.precio}`;
+    atag.textContent = 'Agregar';
+    
+
+    productos.append(divCard)
+    divCard.append(img,divCardBody)
+    divCardBody.append(h5tag,ptagTalle,ptagPrecio,atag)
+
+    atag.addEventListener('click',function(e){
+        divCardBody.append(atag)
+        atag.textContent = 'agregado';
+        atag.classList.remove('btn-outline-primary')
+        atag.classList.add('btn-warning')
+    })
+}
+
+const modal = document.querySelector("#modal")
+if (modal){
+    modal.innerHTML +=`
+    <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+
+
+const mCarrito = () => {
+    const modalBody = document.querySelector("#modal .modal .modal-body");
+    if (modalBody) {
+        carrito.forEach((producto) => {
+        const { id, nombre, precio, img, stock, talle } = productos;
+        console.log(modalBody);
+        modalBody.innerHTML += `
+        <div class="modal-contenedor">
+            <div>
+            <img class="img-fluid img-carrito" src="${img}"/>
+            </div>
+        <div>
+            <p>Producto: ${nombre}</p>
+            <p>Precio: ${precio}</p>
+        <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+            </div>
+        </div>
+        `;
+        });
+    }
+}
+function sumaProducto(id){
+    const item = stockProductos.find((productos) => productos.id === id)
+    carrito.push(item)
+    console.log(carrito)
+    mCarrito()
+}
+const botonCarrito = document.querySelector("#botonCarrito")
+if (botonCarrito){
+    botonCarrito.innerHTML +=`
+    <button type="button" data-toggle="modal" data-target="#exampleModal"><img src="../img/carrito-de-compras.png" class="imgBoton"></button>`
+}
+botonCarrito.addEventListener("click", sumaProducto)
+
+// let productosSeleccionados =[];
+    // let productoElegido = {};
+    // productosSeleccionados.push(productoElegido)
+    // function clickearBoton (e){
+    //     productosSeleccionados.forEach(productoPrecio => {
+    //     precioTotal+= productoPrecio.precio ;
+    //    });            
+    //     if(sessionStorage.getItem('stock')==! null){
+    //         sessionStorage.setItem('precioTotalCompra', precioTotal); 
+    //         atag.textContent = sessionStorage.getItem('stock');
+
+    //     }else{
+    //         atag.textContent = sessionStorage.getItem('stock');
+    //     }
+    // }
+    // precioCompra.innerHTML= precioTotal;
+    
+            // if(sessionStorage.getItem('stock') == null ){
+        //     sessionStorage.setItem('stock',cantidadStock)
+        //     divCardBody.append(pSinStock)
+        //     atag.textContent = 'CON STOCK';
+        //     atag.classList.remove('btn-outline-primary')
+        //     atag.classList.add('btn-warning')
+        // }else{
+        //     sessionStorage.get('stock')
+        // 
+        // }
+// }
+
+// function sumaProducto(id){
+//     const item = stockProductos.find((productos) => productos.id === id)
+//     carrito.push (item) 
+// }
 
 // sessionStorage.setItem('CantidadTotalDeProductos',totalCantidad)
 // stockProductos.forEach((prod) => {
@@ -29,104 +159,4 @@ let totalCantidad = 20
 //     `;
 //     }
 // });
-let precioTotal = 1;
-
-for(let producto of stockProductos){
-    
-
-    const divCard = document.createElement('div');
-    const img = document.createElement('img');
-
-    const divCardBody = document.createElement('div');
-
-    const h5tag = document.createElement('h5');
-
-    const ptagTalle = document.createElement('p');
-    const ptagPrecio = document.createElement('p');
-    const pSinStock = document.createElement('p');
-    const atag = document.createElement('a');
-
-    divCard.classList.add('card','text-dark','mt-5')
-    img.classList.add('card-img-top','mt-2')
-    img.src = `${producto.img}`;
-    divCardBody.classList.add('card-body');
-    h5tag.classList.add('card-title');
-    ptagTalle.classList.add('card-text');
-    ptagPrecio.classList.add('card-text');
-    atag.classList.add('btn','btn-outline-primary')
-    pSinStock.innerHTML= "al carrito"
-
-    h5tag.innerHTML= `${producto.nombre}`;
-    ptagTalle.innerHTML= `${producto.talle}`;
-    ptagPrecio.innerHTML= `${producto.precio}`;
-    atag.textContent = 'Agregar';
-    // let productosSeleccionados =[];
-    // let productoElegido = {};
-    // productosSeleccionados.push(productoElegido)
-    // function clickearBoton (e){
-    //     productosSeleccionados.forEach(productoPrecio => {
-    //     precioTotal+= productoPrecio.precio ;
-    //    });            
-    //     if(sessionStorage.getItem('stock')==! null){
-    //         sessionStorage.setItem('precioTotalCompra', precioTotal); 
-    //         atag.textContent = sessionStorage.getItem('stock');
-
-    //     }else{
-    //         atag.textContent = sessionStorage.getItem('stock');
-    //     }
-    // }
-    // precioCompra.innerHTML= precioTotal;
-    
-
-    productos.append(divCard)
-    divCard.append(img,divCardBody)
-    divCardBody.append(h5tag,ptagTalle,ptagPrecio,atag)
-
-    atag.addEventListener('click',function(e){
-        divCardBody.append(pSinStock)
-        atag.textContent = 'Agregado';
-        atag.classList.remove('btn-outline-primary')
-        atag.classList.add('btn-warning')
-})
-
-        // if(sessionStorage.getItem('stock') == null ){
-        //     sessionStorage.setItem('stock',cantidadStock)
-        //     divCardBody.append(pSinStock)
-        //     atag.textContent = 'CON STOCK';
-        //     atag.classList.remove('btn-outline-primary')
-        //     atag.classList.add('btn-warning')
-        // }else{
-        //     sessionStorage.get('stock')
-        // 
-        // }
-    
-}
-// onclick=sumaProducto(id)
-function sumaProducto(id){
-    const item = stockProductos.find((productos) => productos.id === id)
-    carrito.push (item) 
-    sessionStorage.setItem('productosElegidos',carrito.JSONStringify())
-    mCarrito() 
-}
-const mCarrito = () => {
-    const modalBody = document.querySelector(".modal .modal-body");
-    if (modalBody) {
-        modalBody.innerHTML = "";
-        carrito.forEach((prod) => {
-        const { id, nombre, precio, desc, img, cantidad } = prod;
-        console.log(modalBody);
-        modalBody.innerHTML += `
-        <div class="modal-contenedor">
-            <div>
-            <img class="img-fluid img-carrito" src="${img}"/>
-            </div>
-        <div>
-            <p>Producto: ${nombre}</p>
-            <p>Precio: ${precio}</p>
-        <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
-            </div>
-        </div>
-        `;
-        });
-    }
-}
+// 
